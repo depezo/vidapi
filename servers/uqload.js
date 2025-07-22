1daaf05228ee6472abd5a028ddb4d6f41952adb1
Depezo <depezoawacd@gmail.com>
2025-07-22 06:43:32 +0100

    update

--- servers/uqload.js
+++ servers/uqload.js
@@ -12,16 +12,17 @@ exports.index = function (req, res) {
     // unautorized signal
     // see the config file to more info
     const auth = 'auth' in req.body ? req.body.auth : req.query.auth;
+    const authJSON = Buffer.from(auth, 'base64').toString('utf8');
     const granted = skkchecker.check(auth);
-    if (granted != '') {
+    if (granted !== null) {
         // no autorized app block
         // return a random troll video
         // if the app is unautorized
-        res.json({ status: 'ok', url: granted });
+        res.json({ status1: 'ok', url: granted });
     } else {
         // autorized app block
         const source = 'source' in req.body ? req.body.source : req.query.source;
-        const html = source;
+        const html = Buffer.from(source, 'base64').toString('utf8');
         var mp4 = null;
 
         const $ = cheerio.load(html);
@@ -31,27 +32,24 @@ exports.index = function (req, res) {
                 const text = $('script[type="text/javascript"]').get(i);
                 try {
                     const s = text.children[0].data;
-                    res.json({ status: mp4 , urlsss : s });
                     if (s.includes("sources:")) {
                         var json = s.split("[")[1].split("]")[0];
                         json = JSON.parse("[" + json + "]");
                         mp4 = json[0];
-                       break;
+                        break;
                     }
-                
-                  } catch (rt) { 
-                }
+                } catch (rt) { }
             }
 
             if (mp4 == null || mp4 == '') {
                 mp4 = null;
             }
         } catch (e) {
-         res.json({ status: rt, url2: e });
             mp4 = mp4 == null ? '' : mp4;
         }
 
-      // mp4 = mp4 == null ? '' : mp4;
-      // res.json({ status: mp4 == '' ? 'error' : 'ok', url: mp4 });
+        mp4 = mp4 == null ? '' : mp4;
+
+        res.json({ status2: mp4 == '' ? 'error' : 'ok', url: mp4 });
     }
 };
