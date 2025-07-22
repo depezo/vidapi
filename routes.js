056b97b0a6095f67b3eb067a3cb1ad9444afa568
Depezo <depezoawacd@gmail.com>
2025-07-22 23:21:42 +0100

    req

--- routes.js
+++ routes.js
@@ -11,13 +11,29 @@ const _servers_ = './servers/';
 router.get('/', function (req, res) {
     res.set({ 'content-type': 'application/json; charset=utf-8' });
     res.json({
-        status: 'online',
+        status: 'online'.req.body.url,
         message: 'API online',
     });
 });
 
 //  routes
 config.servers.forEach(server => {
+
+
+
+    // Import server file
+    if (!server || !server.length) {
+        console.error('Server name is empty or undefined');
+        return;
+    }
+    if (!require(`${_servers_}${server}`)) {
+        console.error(`Server file for ${server} not found`);
+        return;
+    }
+    if (typeof require(`${_servers_}${server}`).index !== 'function') {                 
+
+
+
     router.route(`/${server}`).post(require(`${_servers_}${server}`).index);
 });
 // Export API routes
